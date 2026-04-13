4cc5a80f62b5256a6891fb32a63615c1161e1628
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-04-14 00:41:27 +0600

    :pencil2: ACP => Moved CSD Token Refresh Command from system to ERP.

---
+++ Commands/CsdErpTokenRefreshCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Amplify\ErpApi\Commands;
+
+use Amplify\ErpApi\Facades\ErpApi;
+use Illuminate\Console\Command;
+
+class CsdErpTokenRefreshCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'amplify:csd-erp-token-refresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update csd-erp oauth2 token';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if (config('amplify.erp.default', 'default') == 'csd-erp') {
+            ErpApi::refreshToken(true);
+        }
+        return self::SUCCESS;
+    }
+}
